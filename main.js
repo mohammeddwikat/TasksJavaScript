fa49b70ef58601acb497f792f6bc50020e3bd243
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2021-01-20 10:42:28 +0200

    task-js-1 : Add task1 js files with ignoring the modules files

---
+++ main.js
@@ -0,0 +1,37 @@
+const { User } = require('./user.js');
+
+
+const readPlain = (file) => {
+    const csv = require('csv-parser');
+    const fs = require('fs');
+    let arr = [];
+
+    fs.createReadStream(file)
+    .pipe(csv())
+    .on('data', (row) => {
+        let a = new User();
+        Object.assign(a,row);
+        arr.push(a);
+    })
+    .on('end', () => {
+        saveToFile('saveToFile.json',arr);
+    });
+}
+
+const  saveToFile = (file, arr) => {
+    const fs = require('fs');
+    data = JSON.stringify(arr);
+    fs.writeFileSync(file, data);
+}
+
+const readJsonFile = (file) => {
+    const fs = require('./saveToFile.json');
+    for(let i in fs){
+        console.log(fs[i]);
+    }
+}
+
+
+
+readPlain("MOCK_DATA.csv");
+readJsonFile("saveToFile.json");
