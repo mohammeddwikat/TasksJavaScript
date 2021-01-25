223e0cbdb51d39344e24518cdd56d811963176d8
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2021-01-25 19:56:56 +0200

    task-js-1 : Apply Prettier format

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-const { User } = require('./user.js');
-
+const { User } = require("./user.js");
 
 const readPlain = (file) => {
-    const csv = require('csv-parser');
-    const fs = require('fs');
-    let arr = [];
+  const csv = require("csv-parser");
+  const fs = require("fs");
+  let arr = [];
 
-    fs.createReadStream(file)
+  fs.createReadStream(file)
     .pipe(csv())
-    .on('data', (row) => {
-        let a = new User();
-        Object.assign(a,row);
-        arr.push(a);
+    .on("data", (row) => {
+      let a = new User();
+      Object.assign(a, row);
+      arr.push(a);
     })
-    .on('end', () => {
-        saveToFile('saveToFile.json',arr);
+    .on("end", () => {
+      saveToFile("saveToFile.json", arr);
     });
-}
+};
 
-const  saveToFile = (file, arr) => {
-    const fs = require('fs');
-    data = JSON.stringify(arr);
-    fs.writeFileSync(file, data);
-}
+const saveToFile = (file, arr) => {
+  const fs = require("fs");
+  data = JSON.stringify(arr);
+  fs.writeFileSync(file, data);
+};
 
 const readJsonFile = (file) => {
-    const fs = require('./saveToFile.json');
-    for(let i in fs){
-        console.log(fs[i]);
-    }
-}
-
-
+  const fs = require("./saveToFile.json");
+  for (let i in fs) {
+    console.log(fs[i]);
+  }
+};
 
 readPlain("MOCK_DATA.csv");
 readJsonFile("saveToFile.json");
--- user.js
+++ user.js
@@ -1,15 +1,24 @@
-class User{
-    constructor(id, first_name, last_name, email, gender, ip_address, color, parentId){
-        this.id = id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-        this.gender = gender;
-        this.ip_address = ip_address;
-        this.ip_address = ip_address;
-        this.color = color;
-        this.parentId = parentId;
-    }
+class User {
+  constructor(
+    id,
+    first_name,
+    last_name,
+    email,
+    gender,
+    ip_address,
+    color,
+    parentId
+  ) {
+    this.id = id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.gender = gender;
+    this.ip_address = ip_address;
+    this.ip_address = ip_address;
+    this.color = color;
+    this.parentId = parentId;
+  }
 }
 
-module.exports =  {User};
+module.exports = { User };
