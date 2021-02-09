d3a580a60682a151bfade88fb823f295f21fd575
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2021-02-09 13:16:59 +0200

    task-js-2-part-3 : Apply all rules that was asked me

--- dateTesting.js
+++ dateTesting.js
@@ -1,47 +1,28 @@
+const constantsModule = require("./constants.js");
+
+const monthsMMMFormat = constantsModule.MMMFormat;
+const monthsMMMMFormat = constantsModule.MMMMFormat;
+
+/** Detect if the amount in hours or in days to add it on a date object. */
 String.prototype.hoursOrDays = function () {
-  let str = this;
-  if (str[str.length - 1].toUpperCase() == "D") {
+  const amount = this;
+  if (amount[amount.length - 1].toUpperCase() == "D") {
     return "day";
-  } else if (str[str.length - 1] == "H") {
+  } else if (amount[amount.length - 1] == "H") {
     return "hour";
   } else {
     return new Error("Invalid Input");
   }
 };
 
+/**
+ * convert date as string into date object.
+ * @param {string} format - The date as string wants to convert it.
+ */
 String.prototype.formatDate = function (format) {
-  let arrKeys = format.split(/[:/-\s]/);
-  let arrValues = this.split(/[:/-\s]/);
-  let MMM = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Abr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Seb",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let MMMM = [
-    "January",
-    "February",
-    "March",
-    "Abril",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let date = this;
-  let ht = {
+  const arrOfTermsDateKeys = format.split(/[:/-\s]/);
+  const arrOfTermsDateValues = this.split(/[:/-\s]/);
+  let hashTable = {
     year: 1,
     month: 1,
     days: 1,
@@ -49,175 +30,181 @@ String.prototype.formatDate = function (format) {
     minutes: 0,
     seconds: 0,
   };
-  //console.log(arrKeys);
-  //console.log(arrValues);
-  for (let i in arrKeys) {
-    if (arrKeys[i].toUpperCase() === "Y") {
-      ht.year = arrValues[i];
-    } else if (arrKeys[i].toUpperCase() === "YY") {
-      ht.year = arrValues[i];
-    } else if (arrKeys[i] === "M") {
-      ht.month = arrValues[i];
-    } else if (arrKeys[i] === "MM") {
-      ht.month = arrValues[i];
-    } else if (arrKeys[i] === "MMM") {
-      ht.month = MMM.indexOf(arrValues[i]) + 1;
-    } else if (arrKeys[i] === "MMMM") {
-      ht.month = MMMM.indexOf(arrValues[i]) + 1;
-    } else if (arrKeys[i].toUpperCase() === "D") {
-      ht.days = arrValues[i];
-    } else if (arrKeys[i].toUpperCase() === "DD") {
-      ht.days = arrValues[i];
-    } else if (arrKeys[i] == "H") {
-      ht.hours = arrValues[i];
-    } else if (arrKeys[i] == "HH") {
-      ht.hours = arrValues[i];
-    } else if (arrKeys[i] == "m") {
-      ht.minutes = arrValues[i];
-    } else if (arrKeys[i] == "mm") {
-      ht.minutes = arrValues[i];
-    } else if (arrKeys[i].toUpperCase() === "S") {
-      ht.seconds = arrValues[i];
-    } else if (arrKeys[i].toUpperCase() === "SS") {
-      ht.seconds = arrValues[i];
-    } else if (arrKeys[i] === "a") {
-      if (arrValues[i] == "p" && ht.hours != 12) {
-        ht.hours = parseInt(ht.hours) + 12;
-      } else if (arrValues[i] == "a" && ht.hours == 12) {
-        ht.hours = 0;
-      }
-    } else if (arrKeys[i] === "A") {
-      if (arrValues[i] == "P" && ht.hours != 12) {
-        ht.hours = parseInt(ht.hours) + 12;
-      } else if (arrValues[i] == "A" && ht.hours == 12) {
-        ht.hours = 0;
-      }
-    } else if (arrKeys[i] === "aa") {
-      if (arrValues[i] == "pm" && ht.hours != 12) {
-        ht.hours = parseInt(ht.hours) + 12;
-      } else if (arrValues[i] == "am" && ht.hours == 12) {
-        ht.hours = 0;
-      }
-    } else if (arrKeys[i] === "AA") {
-      if (arrValues[i] == "PM" && ht.hours != 12) {
-        ht.hours = parseInt(ht.hours) + 12;
-      } else if (arrValues[i] == "AM" && ht.hours == 12) {
-        ht.hours = 0;
-      }
-    } else {
-      return new Error("Invalid Input");
+  let termDateFormat;
+  for (let pointer in arrOfTermsDateKeys) {
+    termDateFormat = arrOfTermsDateKeys[pointer];
+    switch (termDateFormat) {
+      case "Y":
+      case "y":
+      case "YY":
+      case "yy":
+        hashTable.year = arrOfTermsDateValues[pointer];
+        break;
+      case "M":
+      case "MM":
+        hashTable.month = arrOfTermsDateValues[pointer];
+        break;
+      case "MMM":
+        hashTable.month =
+          monthsMMMFormat.indexOf(arrOfTermsDateValues[pointer]) + 1;
+        break;
+      case "MMMM":
+        hashTable.month =
+          monthsMMMMFormat.indexOf(arrOfTermsDateValues[pointer]) + 1;
+        break;
+      case "D":
+      case "d":
+      case "DD":
+      case "dd":
+        hashTable.days = arrOfTermsDateValues[pointer];
+        break;
+      case "H":
+      case "HH":
+        hashTable.hours = arrOfTermsDateValues[pointer];
+        break;
+      case "m":
+      case "mm":
+        hashTable.minutes = arrOfTermsDateValues[pointer];
+        break;
+      case "s":
+      case "S":
+      case "ss":
+      case "SS":
+        hashTable.seconds = arrOfTermsDateValues[pointer];
+        break;
+      case "a":
+      case "aa":
+      case "A":
+      case "AA":
+        if (
+          (arrOfTermsDateValues[pointer] == "p" ||
+            arrOfTermsDateValues[pointer] == "P" ||
+            arrOfTermsDateValues[pointer] == "pm" ||
+            arrOfTermsDateValues[pointer] == "PM") &&
+          hashTable.hours != 12
+        ) {
+          hashTable.hours = parseInt(hashTable.hours) + 12;
+        } else if (
+          (arrOfTermsDateValues[pointer] == "a" ||
+            arrOfTermsDateValues[pointer] == "A" ||
+            arrOfTermsDateValues[pointer] == "am" ||
+            arrOfTermsDateValues[pointer] == "AM") &&
+          hashTable.hours == 12
+        ) {
+          hashTable.hours = 0;
+        }
+        break;
+      default:
+        return new Error("Invalid Input");
     }
   }
-
   return (
-    ht.month +
+    hashTable.month +
     "/" +
-    ht.days +
+    hashTable.days +
     "/" +
-    ht.year +
+    hashTable.year +
     " " +
-    ht.hours +
+    hashTable.hours +
     ":" +
-    ht.minutes +
+    hashTable.minutes +
     ":" +
-    ht.seconds
+    hashTable.seconds
   );
 };
 
+/**
+ * return the characters on a string based on a specific pattern.
+ * @param {string} str - The string contains characters that we need to find it t add it on a string date.
+ * @param {object} pattern - The object contains the chars that we want to extract it
+ * @param {string} modifier  - Modifiers are used to perform case-insensitive and global searches
+ */
+const findSeparators = (str, pattern, modifier) => {
+  const regularExpression = new RegExp(pattern, modifier);
+  return str.match(regularExpression);
+};
+
+/**
+ * @param {object} termsDate - Contains the terms of the date
+ * check if the format date contains Meridiem
+ * */
+const ifMeridiemExist = (termsDate) => {
+  return (
+    termsDate.includes("a") ||
+    termsDate.includes("A") ||
+    termsDate.includes("aa") ||
+    termsDate.includes("AA")
+  );
+};
+
+/**
+ * Convert date object to a string based on a specific format.
+ * @param {string} str - The format of the date.
+ */
 Date.prototype.format = function (str) {
-  let MMM = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Abr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Seb",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let MMMM = [
-    "January",
-    "February",
-    "March",
-    "Abril",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let date = this;
+  const date = this;
   let finalValue = "";
   let index = 0;
+  let hour;
+  let mon;
+  let min;
+  let sec;
+  let day;
 
-  const searchPattern = (str, pattern, modifier) => {
-    let rE = new RegExp(pattern, modifier);
-    return str.match(rE);
-  };
-  const searchMeridiem = () => {
-    if (
-      arr.includes("a") ||
-      arr.includes("A") ||
-      arr.includes("aa") ||
-      arr.includes("AA")
-    ) {
-      return true;
-    } else {
-      return false;
+  /** Update the pointers on the array of separators and array of terms date */
+  const updateIndexers = () => {
+    if (index < sizeSeparators) {
+      finalValue += separators[index];
+      index++;
     }
   };
-  const addTime = () => {
-    if (arr.includes("m")) {
+
+  /** Complete the time after find the hours based on the format that specified in the str param */
+  const completeTime = () => {
+    if (arrOfTermsDate.includes("m")) {
       finalValue += ":";
       finalValue += date.getMinutes();
-    } else if (arr.includes("mm")) {
-      let min = date.getMinutes();
+    } else if (arrOfTermsDate.includes("mm")) {
+      min = date.getMinutes();
       if (min < 10) {
         min = "0" + min;
       }
       finalValue += ":";
       finalValue += min;
     }
-    if (arr.includes("s") || arr.includes("S")) {
+    if (arrOfTermsDate.includes("s") || arrOfTermsDate.includes("S")) {
       finalValue += ":";
       finalValue += date.getSeconds();
-    } else if (arr.includes("ss") || arr.includes("SS")) {
-      let sec = date.getSeconds();
+    } else if (arrOfTermsDate.includes("ss") || arrOfTermsDate.includes("SS")) {
+      sec = date.getSeconds();
       if (sec < 10) {
         sec = "0" + sec;
       }
       finalValue += ":";
       finalValue += sec;
     }
-    if (arr.includes("a")) {
+    if (arrOfTermsDate.includes("a")) {
       finalValue += " ";
       if (date.getHours() >= 12) {
         finalValue += "p";
       } else {
         finalValue += "a";
       }
-    } else if (arr.includes("A")) {
+    } else if (arrOfTermsDate.includes("A")) {
       finalValue += " ";
       if (date.getHours() >= 12) {
         finalValue += "P";
       } else {
         finalValue += "A";
       }
-    } else if (arr.includes("aa")) {
+    } else if (arrOfTermsDate.includes("aa")) {
       finalValue += " ";
       if (date.getHours() >= 12) {
         finalValue += "pm";
       } else {
         finalValue += "am";
       }
-    } else if (arr.includes("AA")) {
+    } else if (arrOfTermsDate.includes("AA")) {
       finalValue += " ";
       if (date.getHours() >= 12) {
         finalValue += "PM";
@@ -226,106 +213,135 @@ Date.prototype.format = function (str) {
       }
     }
   };
-  let separators = searchPattern(str, /[:\,/-\s]/, "g");
+  const separators = findSeparators(str, /[:\,/-\s]/, "g");
   let sizeSeparators = 0;
   if (separators != null) {
     sizeSeparators = separators.length;
   }
-  //console.log(separators);
-
-  let arr = str.split(/[.\-_\s,:/]/);
-  //console.log(arr);
-
-  for (let i in arr) {
-    if (arr[i].toUpperCase() == "Y") {
-      finalValue += date.getFullYear().toString();
-    } else if (arr[i].toUpperCase() == "YY") {
-      finalValue += (date.getYear() % 100).toString();
-    } else if (arr[i] == "M") {
-      finalValue += (date.getMonth() + 1).toString();
-    } else if (arr[i] == "MM") {
-      let mon = date.getMonth() + 1;
-      if (mon < 10) {
-        mon = "0" + mon;
-      }
-      finalValue += mon.toString();
-    } else if (arr[i] == "MMM") {
-      finalValue += MMM[date.getMonth()];
-    } else if (arr[i] == "MMMM") {
-      finalValue += MMMM[date.getMonth()];
-    } else if (arr[i].toUpperCase() == "D") {
-      finalValue += date.getDate().toString();
-    } else if (arr[i].toUpperCase() == "DD") {
-      let day = date.getDate();
-      if (day < 10) {
-        day = "0" + day;
-      }
-      finalValue += day.toString();
-    } else if (arr[i] == "H") {
-      if (searchMeridiem() == true) {
-        let hour = date.getHours();
-        hour = hour % 12;
-        hour = hour == 0 ? 12 : hour;
-        finalValue += hour;
-      } else {
-        finalValue += date.getHours();
-      }
-      addTime();
-      index++;
-      finalValue += " ";
-      continue;
-    } else if (arr[i] == "HH") {
-      let hour = date.getHours();
-
-      if (searchMeridiem() == true) {
-        hour = hour % 12;
-        hour = hour == 0 ? 12 : hour;
-        if (hour < 10) {
-          hour = "0" + hour;
+  const arrOfTermsDate = str.split(/[.\-_\s,:/]/);
+  let termDateFormat;
+  for (let i in arrOfTermsDate) {
+    termDateFormat = arrOfTermsDate[i];
+    switch (termDateFormat) {
+      case "Y":
+      case "y":
+        finalValue += date.getFullYear().toString();
+        updateIndexers();
+        break;
+      case "YY":
+      case "yy":
+        finalValue += (date.getYear() % 100).toString();
+        updateIndexers();
+        break;
+      case "M":
+        finalValue += (date.getMonth() + 1).toString();
+        updateIndexers();
+        break;
+      case "MM":
+        mon = date.getMonth() + 1;
+        if (mon < 10) {
+          mon = "0" + mon;
         }
-        finalValue += hour;
-      } else {
-        if (hour < 10) {
-          hour = "0" + hour;
+        finalValue += mon.toString();
+        updateIndexers();
+        break;
+      case "MMM":
+        finalValue += monthsMMMFormat[date.getMonth()];
+        updateIndexers();
+        break;
+      case "MMMM":
+        finalValue += monthsMMMMFormat[date.getMonth()];
+        updateIndexers();
+        break;
+      case "d":
+      case "D":
+        finalValue += date.getDate().toString();
+        updateIndexers();
+        break;
+      case "DD":
+      case "dd":
+        day = date.getDate();
+        if (day < 10) {
+          day = "0" + day;
         }
-        finalValue += hour;
-      }
-      addTime();
-      finalValue += " ";
-      index++;
-      continue;
-    } else if (
-      arr[i].toUpperCase() != "S" &&
-      arr[i].toUpperCase() != "SS" &&
-      arr[i] != "m" &&
-      arr[i] != "mm" &&
-      arr[i] != "a" &&
-      arr[i] != "A" &&
-      arr[i] != "aa" &&
-      arr[i] != "AA"
-    ) {
-      return new Error("Invalid Format");
-    } else {
-      index++;
-      continue;
-    }
-    if (index < sizeSeparators) {
-      finalValue += separators[index];
-      index++;
+        finalValue += day.toString();
+        updateIndexers();
+        break;
+      case "H":
+        if (ifMeridiemExist(arrOfTermsDate) == true) {
+          hour = date.getHours();
+          hour = hour % 12;
+          hour = hour == 0 ? 12 : hour;
+          finalValue += hour;
+        } else {
+          finalValue += date.getHours();
+        }
+        completeTime();
+        index++;
+        finalValue += " ";
+        break;
+      case "HH":
+        hour = date.getHours();
+
+        if (ifMeridiemExist(arrOfTermsDate) == true) {
+          hour = hour % 12;
+          hour = hour == 0 ? 12 : hour;
+          if (hour < 10) {
+            hour = "0" + hour;
+          }
+          finalValue += hour;
+        } else {
+          if (hour < 10) {
+            hour = "0" + hour;
+          }
+          finalValue += hour;
+        }
+        completeTime();
+        finalValue += " ";
+        index++;
+        break;
+      case "S":
+      case "SS":
+      case "s":
+      case "ss":
+      case "mm":
+      case "m":
+      case "a":
+      case "A":
+      case "aa":
+      case "AA":
+        index++;
+        break;
+      default:
+        return new Error("Invalid Format");
     }
   }
-
   return finalValue.trim();
 };
 
-const dateToString = (dateObj, formatStr) => {
-  return dateObj.format(formatStr);
+/**
+ * convert a date object to a string with a specific format.
+ * @param {Date} date - Date object.
+ * @param {string} format - The format that we want the date show as it.
+ */
+const dateToString = (date, format) => {
+  return date.format(format);
 };
 
-const stringToDate = (date, foramt) => {
-  return new Date(date.formatDate(foramt));
+/**
+ * convert a string as date to object date.
+ * @param {string} date - string represent a date.
+ * @param {string} format - The format that the date follow it.
+ */
+const stringToDate = (date, format) => {
+  return new Date(date.formatDate(format));
 };
 
+/**
+ * add days or hours on a date object.
+ * @param {Date} date - Date object that we want to add hours or days on it.
+ * @param {string} amount - The amount that we want to add it.
+ */
 const add = (date, amount) => {
   let str;
   if (amount.hoursOrDays() == "day") {
